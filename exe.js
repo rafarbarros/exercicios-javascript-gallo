cc60c35ebdf3a66c43bc9346b7f6ffc75d474372
rafarbarros <rr-barros@hotmail.com>
2023-11-25 16:25:56 -0300

    Exercicio Javascript Estrutura de dados Gallo

---
+++ exe.js
@@ -0,0 +1,39 @@
+class Navegacao {
+    constructor() {
+      this.historico = [];
+    }
+  
+    // Método para empilhar um novo endereço
+    empilhar(endereco) {
+      this.historico.push(endereco);
+    }
+  
+    // Método para desempilhar o endereço mais recente
+    desempilhar() {
+      if (this.historico.length === 0) {
+        console.log("Histórico de navegação vazio");
+        return null;
+      }
+      return this.historico.pop();
+    }
+  
+    // Método para exibir o histórico de navegação
+    exibirHistorico() {
+      console.log("Histórico de Navegação:", this.historico);
+    }
+  }
+  
+  // Exemplo de uso
+  const historicoNavegacao = new Navegacao();
+  
+  historicoNavegacao.empilhar("https://www.google.com");
+  historicoNavegacao.empilhar("https://www.linkedin.com");
+  historicoNavegacao.empilhar("https://www.github.com");
+  
+  historicoNavegacao.exibirHistorico();
+  
+  const enderecoAtual = historicoNavegacao.desempilhar();
+  console.log("Endereço Atual:", enderecoAtual);
+  
+  historicoNavegacao.exibirHistorico();
+  
